c68d5dba7833a8d496002cd7d947f3299bd5d6a0
Jack Murphy <jmurphy.web@gmail.com>
2016-11-12 10:15:17 +0000

    add basic inject module and test

---
+++ injector.js
@@ -0,0 +1,34 @@
+/* --- INJECT MODULE --- */
+
+var http = require('http')
+
+var inject = (cb) => {
+  http.get({
+    hostname: 'localhost',
+    port: 8080,
+    path: '/',
+    agent: false
+  }, (res) => {
+    res.on('data', data => {
+      cb({ payload: data.toString() })
+    })
+  })
+}
+
+
+/* --- TEST --- */
+var tape =  require('tape')
+
+// start the server:
+var server = require('./server.js')
+
+tape('check route', t => {
+  inject(response => {
+    t.equal(response.payload, 'It Works!!',  'it does work')
+    t.end()
+  })
+})
+
+tape('teardown', t => {
+  server.close(t.end)
+})
