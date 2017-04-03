02b6c5c49b931ed07fd0a03e28d7e7163810e265
WilliamSavage <willsavage@hotmail.com>
2017-04-03 22:20:58 +0100

    adds script for testRunner

--- tests/inject.js
+++ tests/inject.js
@@ -1,7 +1,29 @@
-const inject = (cb) => {
-  // Code...
-  // what core node modules might you need here?
-  cb('response')
+const http = require('http')
+
+const inject = (options, cb) => {
+  options = (typeof options === 'string' ? { url: options } : options)
+
+  http.get({
+    hostname: 'localhost',
+    port: 4000,
+    headers: options.headers,
+    path: options.url,
+    agent: false
+  }, (res, err) => {
+    if (err) {
+      console.log(err)
+    }
+
+    let str = ''
+
+    res.on('data', data => {
+      str += data
+    })
+
+    res.on('end', () => {
+      cb({ payload: str, code: res.statusCode })
+    })
+  })
 }
 
 module.exports = inject
